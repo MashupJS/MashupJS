cb31d1ed46744f6162e829bd1fe1734ede029da1
robertdunaway <robertdunaway@usa.net>
2015-04-27 10:52:11 -0400

    Working on new gulp implementation.

--- src/Mashup.UI.Core/gulpfile.js
+++ src/Mashup.UI.Core/gulpfile.js
@@ -1,9 +1,22 @@
 ﻿var gulp = require('gulp')
+    , clean = require('gulp-clean')
     , uglify = require('gulp-uglify')
     , rename = require('gulp-rename')
     , sourcemaps = require('gulp-sourcemaps')
     , concat = require('gulp-concat');
 
+var distFolder = 'dist';
+var srcFolder = 'src';
+
+gulp.task('clean', function () {
+    return gulp.src(distFolder, { read: false })
+        .pipe(clean());
+});
+
+gulp.task('copy', ['clean'], function () {
+    return gulp.src(srcFolder + '/**/*')
+    .pipe(gulp.dest(distFolder))
+});
 
 
 gulp.task('uglify:apps', function () {
@@ -16,12 +29,9 @@ gulp.task('uglify:apps', function () {
     //    .pipe(sourcemaps.write('./'))
     //    .pipe(gulp.dest('core/apps/'))
     //;
-
-
-
-
+    
 });
 
-
-gulp.task('default', ['uglify:apps']);
+gulp.task('default', ['clean', 'copy']);
+//gulp.task('default', ['uglify:apps']);
 
